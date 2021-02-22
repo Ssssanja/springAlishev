27fd38cf518b98cb5b1411d59177586f4883bbb2
alexandrito <7978005@gmail.com>
2021-02-22 14:18:31 +0300

    Scope - prototype

--- src/main/java/by/liukevich/springcourse/TestSpring.java
+++ src/main/java/by/liukevich/springcourse/TestSpring.java
@@ -7,17 +7,15 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "ApplicationContext.xml"
         );
-//        Music music = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         boolean comparsion = firstMusicPlayer==secondMusicPlayer;
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
         System.out.println(comparsion);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-//        firstMusicPlayer.playMusic();
-//        System.out.println(firstMusicPlayer.getName());
-//        System.out.println(firstMusicPlayer.getVolume());
         context.close();
     }
 }
