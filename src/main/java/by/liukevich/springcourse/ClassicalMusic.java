9ee1897ccede879fa268ff6274d7b3eb7e6ba05f
alexandrito <7978005@gmail.com>
2021-02-22 14:58:46 +0300

    Factory-method

--- src/main/java/by/liukevich/springcourse/ClassicalMusic.java
+++ src/main/java/by/liukevich/springcourse/ClassicalMusic.java
@@ -1,6 +1,11 @@
 package by.liukevich.springcourse;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic () {}
+
+    public static ClassicalMusic getClassicalMusic () {
+        return new ClassicalMusic();
+    }
     @Override
     public String getSong() {
         return "Hungarian Rapsody";
--- src/main/java/by/liukevich/springcourse/TestSpring.java
+++ src/main/java/by/liukevich/springcourse/TestSpring.java
@@ -9,8 +9,6 @@ public class TestSpring {
         );
 
         ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
         System.out.println(classicalMusic.getSong());
 //        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 //        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
