7e13fa08dd0805575da026249204fe07db416c18
alexandrito <7978005@gmail.com>
2021-02-22 14:45:14 +0300

    Destroy-method, Init-method

--- src/main/java/by/liukevich/springcourse/ClassicalMusic.java
+++ src/main/java/by/liukevich/springcourse/ClassicalMusic.java
@@ -5,4 +5,10 @@ public class ClassicalMusic implements Music{
     public String getSong() {
         return "Hungarian Rapsody";
     }
+    public void doMyInit () {
+        System.out.println("Doing my initialisation");
+    }
+    public void doMyDestroy () {
+        System.out.println("Doing my destroying");
+    }
 }
--- src/main/java/by/liukevich/springcourse/TestSpring.java
+++ src/main/java/by/liukevich/springcourse/TestSpring.java
@@ -7,15 +7,18 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "ApplicationContext.xml"
         );
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        boolean comparsion = firstMusicPlayer==secondMusicPlayer;
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
-        System.out.println(comparsion);
+
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        boolean comparsion = firstMusicPlayer==secondMusicPlayer;
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+//        System.out.println(comparsion);
         context.close();
     }
 }
